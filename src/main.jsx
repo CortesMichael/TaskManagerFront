e24978a797e292de9bb945eb04cf68e59fc90b80
KauaHenrique06 <kauahcarmo@gmail.com>
2025-05-11 20:59:59 -0300

    context de usuário criado

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { Router } from "./routes/Router.jsx"
 import { RouterProvider } from 'react-router-dom'
+import { UserProvider } from './context/UserContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router = {Router} />
+    <UserProvider>
+        <RouterProvider router = {Router} />
+    </UserProvider>
   </StrictMode>,
 )
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,10 +1,15 @@
+import { useUserContext } from "../../context/UserContext";
 import style from "./Login.module.css"
 
 export const Login = () => {
 
+    const {loggedUser} = useUserContext();
+
     return(
         <div className={style.page}>
-            <h1>ta</h1>
+            <h1>
+                {loggedUser == null && "funcionou!!"}
+            </h1>
         </div>
     );
 }
